bb6fbedf14da6b33f1402721e5119cca248031d8
Amin Aghalari <51974151+aa2683@users.noreply.github.com>
2024-05-30 10:21:12 -0500

    Update classifier_app.py

--- classifier_app.py
+++ classifier_app.py
@@ -8,10 +8,12 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import ConfusionMatrixDisplay, RocCurveDisplay, PrecisionRecallDisplay, confusion_matrix
 from sklearn.metrics import precision_score, recall_score
-import matplotlib.pyplot as plt
+
 
 # st.set_option('deprecation.showPyplotGlobalUse', False)
+st.write('fixing')
 
+import matplotlib.pyplot as plt
 def main():
     st.title("Binary Classification Web App")
     st.sidebar.title("Binary Classification Web App")
