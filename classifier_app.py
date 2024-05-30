2edcfd29584d9d394581a6a8498e0e6d16209cd6
Amin Aghalari - a0a0iuh <Amin.Aghalari@walmart.com>
2024-05-30 13:49:10 -0500

    Update classifier_app.py

--- classifier_app.py
+++ classifier_app.py
@@ -76,10 +76,11 @@ def main():
     st.sidebar.subheader("Choose Classifier")
     classifier = st.sidebar.selectbox("dataset", ("Support Vector Machine (SVM)", "Logistic Regression", "Random Forest", "K-nearest neighbors", "Gaussian Naive Bayes"))
 
-    if st.sidebar.button("Classify", key='classify'):
-        if classifier == 'Gaussian Naive Bayes':
-            
-            metrics = st.sidebar.multiselect("What metrics to plot?", ('Confusion Matrix', 'ROC Curve', 'Precision-Recall Curve'))
+    
+    if classifier == 'Gaussian Naive Bayes':
+        
+        metrics = st.sidebar.multiselect("What metrics to plot?", ('Confusion Matrix', 'ROC Curve', 'Precision-Recall Curve'))
+        if st.sidebar.button("Classify", key='classify'):
             st.subheader("Gaussian Naive Bayes")
             model = GaussianNB()
             model.fit(x_train, y_train)
@@ -90,10 +91,10 @@ def main():
             st.write("Recall: ", recall_score(y_test, y_pred, labels=class_names).round(2))
             plot_metrics(model, metrics, y_test, y_pred)
 
-        if classifier == 'K-nearest neighbors':
-            n_neighbors = st.sidebar.slider("number of neighbors", 12, 30, key='n_neigbbours')
-            metrics = st.sidebar.multiselect("What metrics to plot?", ('Confusion Matrix', 'ROC Curve', 'Precision-Recall Curve'))
-            
+    if classifier == 'K-nearest neighbors':
+        n_neighbors = st.sidebar.slider("number of neighbors", 12, 30, key='n_neigbbours')
+        metrics = st.sidebar.multiselect("What metrics to plot?", ('Confusion Matrix', 'ROC Curve', 'Precision-Recall Curve'))
+        if st.sidebar.button("Classify", key='classify'):
             st.subheader("K-nearest neighbors")
             model = KNeighborsClassifier(n_neighbors=n_neighbors)
             model.fit(x_train, y_train)
@@ -105,16 +106,16 @@ def main():
             plot_metrics(model, metrics, y_test, y_pred)
 
 
-        if classifier == 'Support Vector Machine (SVM)':
-            st.sidebar.subheader("Model Hyperparameters")
-            #choose parameters
-            C = st.sidebar.number_input("C (Regularization parameter)", 0.01, 10.0, step=0.01, key='C_SVM')
-            kernel = st.sidebar.radio("Kernel", ("rbf", "linear"), key='kernel')
-            gamma = st.sidebar.radio("Gamma (Kernel Coefficient)", ("scale", "auto"), key='gamma')
+    if classifier == 'Support Vector Machine (SVM)':
+        st.sidebar.subheader("Model Hyperparameters")
+        #choose parameters
+        C = st.sidebar.number_input("C (Regularization parameter)", 0.01, 10.0, step=0.01, key='C_SVM')
+        kernel = st.sidebar.radio("Kernel", ("rbf", "linear"), key='kernel')
+        gamma = st.sidebar.radio("Gamma (Kernel Coefficient)", ("scale", "auto"), key='gamma')
 
-            metrics = st.sidebar.multiselect("What metrics to plot?", ('Confusion Matrix', 'ROC Curve', 'Precision-Recall Curve'))
-            
-            
+        metrics = st.sidebar.multiselect("What metrics to plot?", ('Confusion Matrix', 'ROC Curve', 'Precision-Recall Curve'))
+        
+        if st.sidebar.button("Classify", key='classify'):
             st.subheader("Support Vector Machine (SVM) Results")
             model = SVC(C=C, kernel=kernel, gamma=gamma)
             model.fit(x_train, y_train)
@@ -124,14 +125,15 @@ def main():
             st.write("Precision: ", precision_score(y_test, y_pred, labels=class_names).round(2))
             st.write("Recall: ", recall_score(y_test, y_pred, labels=class_names).round(2))
             plot_metrics(model, metrics, y_test, y_pred)
-        
-        if classifier == 'Logistic Regression':
-            st.sidebar.subheader("Model Hyperparameters")
-            C = st.sidebar.number_input("C (Regularization parameter)", 0.01, 10.0, step=0.01, key='C_LR')
-            max_iter = st.sidebar.slider("Maximum number of iterations", 100, 500, key='max_iter')
+    
+    if classifier == 'Logistic Regression':
+        st.sidebar.subheader("Model Hyperparameters")
+        C = st.sidebar.number_input("C (Regularization parameter)", 0.01, 10.0, step=0.01, key='C_LR')
+        max_iter = st.sidebar.slider("Maximum number of iterations", 100, 500, key='max_iter')
 
-            metrics = st.sidebar.multiselect("What metrics to plot?", ('Confusion Matrix', 'ROC Curve', 'Precision-Recall Curve'))
+        metrics = st.sidebar.multiselect("What metrics to plot?", ('Confusion Matrix', 'ROC Curve', 'Precision-Recall Curve'))
 
+        if st.sidebar.button("Classify", key='classify'):
             st.subheader("Logistic Regression Results")
             model = LogisticRegression(C=C, penalty='l2', max_iter=max_iter)
             model.fit(x_train, y_train)
@@ -141,14 +143,15 @@ def main():
             st.write("Precision: ", precision_score(y_test, y_pred, labels=class_names).round(2))
             st.write("Recall: ", recall_score(y_test, y_pred, labels=class_names).round(2))
             plot_metrics(model, metrics, y_test, y_pred)
-        
-        if classifier == 'Random Forest':
-            st.sidebar.subheader("Model Hyperparameters")
-            n_estimators = st.sidebar.number_input("The number of trees in the forest", 100, 5000, step=10, key='n_estimators')
-            max_depth = st.sidebar.number_input("The maximum depth of the tree", 1, 20, step=1, key='max_depth')
-            bootstrap = st.sidebar.radio("Bootstrap samples when building trees", ('True', 'False'), key='bootstrap')
-            metrics = st.sidebar.multiselect("What metrics to plot?", ('Confusion Matrix', 'ROC Curve', 'Precision-Recall Curve'))
+    
+    if classifier == 'Random Forest':
+        st.sidebar.subheader("Model Hyperparameters")
+        n_estimators = st.sidebar.number_input("The number of trees in the forest", 100, 5000, step=10, key='n_estimators')
+        max_depth = st.sidebar.number_input("The maximum depth of the tree", 1, 20, step=1, key='max_depth')
+        bootstrap = st.sidebar.radio("Bootstrap samples when building trees", ('True', 'False'), key='bootstrap')
+        metrics = st.sidebar.multiselect("What metrics to plot?", ('Confusion Matrix', 'ROC Curve', 'Precision-Recall Curve'))
 
+        if st.sidebar.button("Classify", key='classify'):
             st.subheader("Random Forest Results")
             model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth, bootstrap=True, n_jobs=-1)
             model.fit(x_train, y_train)
